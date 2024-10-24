84a44bb16c002b4b7ac16b6f5e329db9330e1a15
MoKlay <137613170+MoKlay@users.noreply.github.com>
2024-10-24 12:10:26 +0000

    3 Лабараторная готова

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
-const fs = require("fs");
+import fs from 'fs'
 
 if (process.argv[2]) {
     fs.readFile(process.argv[2], (err, data) => {
         if (err) {
             console.log('Ошибка чтения', err);
         }
-        let text = data
-        text.forEach(element => {
-            console.log(String.fromCharCode(element+65));
-        });
+        data = String(data.toString())
+        let text = data.replace(/\(\d+,\d+\)/g, '')
+        console.log(data)
+        console.log(text)
+        while (data.includes('(')) {
+            const start = data.indexOf('(')
+            const mass = JSON.parse(`[${data.slice(start + 1, data.indexOf(')'))}]`)
+            const path = text.slice(start - Math.abs(mass[0]), start - Math.abs(mass[0]) + mass[1])
+            text = text.slice(0, start) + path + text.slice(start, text.length)
+            data = data.replace(/\(\d+,\d+\)/, path)
+            console.log(start ,mass ,path, text);
+        }
+        console.log(data)
+        fs.writeFileSync('./newtext.txt', data)
+        const textsize = fs.statSync(process.argv[2]).size
+        const newtextsize = fs.statSync('./newtext.txt').size
+        console.log(textsize, newtextsize);
+        
     })
 } else {
     console.log('Укажите в параметр путь файла');
